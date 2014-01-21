752dc33bc6f4212cfe137374c206d337976ced22
TimoSbrzesny <timo.sbrzesny@gmail.com>
2014-01-21 18:25:40 +0100

    add of first try with discovery

--- GitTest/GitTest/Program.cs
+++ GitTest/GitTest/Program.cs
@@ -10,6 +10,8 @@ namespace GitTest
     {
         static void Main(string[] args)
         {
+            System.Console.WriteLine("This is a simple test");
+            System.Console.ReadLine();
         }
     }
 }
