fafcdab586f3b01964466d08018fb198ccfc0dfa
TimoSbrzesny <timo.sbrzesny@gmail.com>
2014-01-21 19:15:44 +0100

    first test project. Still having some problems to fully get what is ment

---
+++ DiscoveryFirstTest/Client/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.ServiceModel.Discovery;
+
+namespace Client
+{
+    class Program
+    {
+        static EndpointAddress serviceAddress;
+        static void Main()
+        {
+            if (FindService()) 
+                InvokeService();
+        }
+        static bool FindService()
+        {
+            Console.WriteLine("\nFinding Myservice Service ..");
+            DiscoveryClient discoveryClient =
+                  new DiscoveryClient(new UdpDiscoveryEndpoint());
+            var Services =
+                 discoveryClient.Find(new FindCriteria(typeof(Service.IService1)));
+            discoveryClient.Close();
+            if (Services == null)
+            {
+                Console.WriteLine("\nNo services are found.");
+                return false;
+            }
+            else
+            {
+                serviceAddress = Services.Endpoints[0].Address;
+                return true;
+            }
+        }
+        static void InvokeService()
+        {
+            Console.WriteLine("\nInvoking My Service at {0}\n", serviceAddress);
+            Service.Service1 client = new Service.Service1();
+            //client.
+            //client.Endpoint.Address = serviceAddress;
+            //client.GetData(1);
+        }
+    }
+}
---
+++ DiscoveryFirstTest/Host/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.ServiceModel.Discovery;
+
+namespace Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uri baseAddress = new Uri(string.Format("http://{0}:8000/discovery/scenarios/Myservice/{1}/",
+            System.Net.Dns.GetHostName(), Guid.NewGuid().ToString()));
+            Console.WriteLine(baseAddress);
+            using (ServiceHost serviceHost = new ServiceHost(typeof(Service.Service1), baseAddress))
+            {
+                serviceHost.AddServiceEndpoint(typeof(Service.IService1), new WSHttpBinding(), string.Empty);
+                serviceHost.Description.Behaviors.Add(new ServiceDiscoveryBehavior());
+                serviceHost.AddServiceEndpoint(new UdpDiscoveryEndpoint());
+                serviceHost.Open();
+                Console.WriteLine("Press to terminate service.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
